261f6a914d2465b83dd99503f9bf3c7b9afaa8dd
LeviRocha <110108655+LeviRochaGarciaLeite@users.noreply.github.com>
2026-02-23 19:56:39 +0000

    exercicio feito

---
+++ src/main/java/app/controllers/GenerosController.java
@@ -0,0 +1,54 @@
+package app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import app.model.Genero;
+import app.repository.GeneroRepository;
+
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@RestController
+@RequestMapping("/generos")
+public class GenerosController {
+    @Autowired
+    private GeneroRepository generoRepo;
+
+    @GetMapping
+    public Iterable<Genero> list() {
+        return generoRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Genero getOne(@PathVariable long id) {
+        return generoRepo.findById(id).get();
+    }
+
+    @PostMapping
+    public Genero insert(@RequestBody Genero novoGenero) {
+        return generoRepo.save(novoGenero);
+    }
+
+    @PutMapping("/{id}")
+    public Genero update(@PathVariable long id, @RequestBody Genero modif){
+        Optional<Genero> busca = generoRepo.findById(id);
+        busca.get().setNome(modif.getNome());
+        return generoRepo.save(busca.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        generoRepo.deleteById(id);
+    }
+}
---
+++ src/main/java/app/model/Genero.java
@@ -0,0 +1,28 @@
+package app.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="generos")
+public class Genero {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+
+    public Genero(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+}
